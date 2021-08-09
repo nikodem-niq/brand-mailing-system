30227ccd1def2fbabb3bf11e6c12cd0fc3c21dad
blvzy <bl.szulik@gmail.com>
2021-08-09 12:30:05 +0200

    fix: take data from .env, nothing stays in code & feat: read alt.txt

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path:'../.env'});
 const express = require('express');
 const nodeMailer = require('nodemailer');
 const app = express();
@@ -17,48 +18,39 @@ fs.readFile('../output/companies.json', (err,file) => {
 
 app.set('views', path.join(__dirname, 'views')); 
 app.get('/send', (req,res) => {
-    let testTransport1 = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'test1',
-            pass: 'test2'
-        }
-    };
-
     let testTransport2 = {
         service: 'gmail',
         auth: {
-            user: 'test1',
-            pass: 'test2'
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS
         }
     }
-
 
     const transporter = nodeMailer.createTransport(testTransport2)
     fs.readFile('views/main.html', function read(err, data) {
         if (err) {
             throw err;
-        }
+        };
         let htmlFile = data.toString();
-        const mailOptions = {
-            from: 'sender',
-            bcc: 'receiver',
-            subject: 'topic',
-            text: 'plain text',
-            html: htmlFile
-        }
-    
-        transporter.sendMail(mailOptions, (err, info) => {
-            console.log(info, err);
-            res.send(info);
+        fs.readFile('views/alt.txt', (err,txt) => {
+            let txtFile = txt.toString();
+            for(company in companies) {
+                const mailOptions = {
+                    from: process.env.EMAIL,
+                    to: companies[company],
+                    subject: 'topic',
+                    text: txtFile,
+                    html: htmlFile
+                }
+            
+                transporter.sendMail(mailOptions, (err, info) => {
+                    console.log(info, err);
+                    res.send(info);
+                })
+            }
         })
     });
-
-
-
 })
-
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
