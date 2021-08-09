e40e3811ccae89bd6265c43f0946070757bc90f1
Nikodem Zychma <nikodemniq@Nikodems-MacBook-Pro.local>
2021-08-09 12:25:47 +0200

    fix: add .env | feat: read txt file

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config({path:'../.env'});
 const express = require('express');
 const nodeMailer = require('nodemailer');
 const app = express();
@@ -5,32 +6,23 @@ const fs = require('fs');
 const path = require('path');
 const port = process.env.PORT || 5000;
 
-let companies = [];
-fs.readFile('../output/companies.json', (err,file) => {
-    let companiesList = JSON.parse(file);
-    for(company in companiesList) {
-        companies.push(companiesList[company].email);
-    }
-    let receivers = companies.join(',');
-})
+let companies = ['shey@riseup.net'];
+// fs.readFile('../output/companies.json', (err,file) => {
+//     let companiesList = JSON.parse(file);
+//     for(company in companiesList) {
+//         companies.push(companiesList[company].email);
+//     }
+//     let receivers = companies.join(',');
+// })
 
 
 app.set('views', path.join(__dirname, 'views')); 
 app.get('/send', (req,res) => {
-    let testTransport1 = {
-        host: 'smtp.ethereal.email',
-        port: 587,
-        auth: {
-            user: 'test1',
-            pass: 'test2'
-        }
-    };
-
     let testTransport2 = {
         service: 'gmail',
         auth: {
-            user: 'test1',
-            pass: 'test2'
+            user: process.env.EMAIL,
+            pass: process.env.EMAIL_PASS
         }
     }
 
@@ -39,19 +31,24 @@ app.get('/send', (req,res) => {
     fs.readFile('views/main.html', function read(err, data) {
         if (err) {
             throw err;
-        }
+        };
         let htmlFile = data.toString();
-        const mailOptions = {
-            from: 'sender',
-            bcc: 'receiver',
-            subject: 'topic',
-            text: 'plain text',
-            html: htmlFile
-        }
-    
-        transporter.sendMail(mailOptions, (err, info) => {
-            console.log(info, err);
-            res.send(info);
+        fs.readFile('views/alt.txt', (err,txt) => {
+            let txtFile = txt.toString();
+            for(company in companies) {
+                const mailOptions = {
+                    from: 'forniteskinsallegro@gmail.com',
+                    to: companies[company],
+                    subject: 'topic',
+                    text: txtFile,
+                    html: htmlFile
+                }
+            
+                transporter.sendMail(mailOptions, (err, info) => {
+                    console.log(info, err);
+                    res.send(info);
+                })
+            }
         })
     });
 
